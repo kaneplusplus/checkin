c39512b1b5419044c6a6463741dc0e1f8b284e4d
kaneplusplus <kaneplusplus@gmail.com>
2020-09-20 18:35:23 -0400

    More bug squashing.

--- R/interval-iterator.r
+++ R/interval-iterator.r
@@ -5,36 +5,44 @@ interval_iter <- function(x, ts_col, start, end, duration, start_loc = TRUE,
   end_loc = TRUE) {
 
   interval_iter <- 
-  next_elem <- function() {
-    if (start > tail(x[[ts_col]], 1)) {
-      stop("StopIteration", call. = FALSE) 
-    } else {
-      end <- start + duration
-      ret <- checkins_in_interval(x, ts_col, start, end, start_loc, end_loc)
-      start <<- end + seconds(1)
-    } 
-    ret
-  }
+    next_elem <- function() {
+      if (start > end) {
+        stop("StopIteration", call. = FALSE) 
+      } else {
+        end <- start + duration
+        ret <- checkins_in_interval(x, ts_col, start, end, start_loc, end_loc)
+        start <<- end + seconds(1)
+      } 
+      ret
+    }
   it <- list(nextElem = next_elem)
   class(it) <- c("abstractiter", "iter")
   it
 }
 
 #' @importFrom utils tail
-#' @importFrom lubridate hour<- minute<- second<-
+#' @importFrom lubridate hour<- minute<- second<- hours 
 #' @export
-hour_checkin_iter <- function(x, ts_col, start_loc = TRUE, end_loc = TRUE) {
+hour_checkin_iter <- function(x, ts_col, end_of_day = TRUE) {
   x <- x[order(x[[ts_col]]),]
   start <- x[[ts_col]][1]
-  hour(start) <- 0
-  minute(start) <- 0
-  second(start) <- 0
-  end <- tail(x[[ts_col]], 1)
-  hour(end) <- 23
-  minute(end) <- 59
-  second(end) <- 59
-  
-  interval_iter(x, ts_col, start, end, hours(1) - seconds(1), start_loc, 
-                end_loc)
+  if (!(minute(start) == 0 && second(start) == 0)) {
+    start <- start + hours(1)
+    minute(start) <- second(start) <- 0
 
+    i <- which.max(x$timestamp <= start)
+    if (i > 1) {
+      x <- x[-( (seq_len(i) - 1)[-1] ),]
+    }
+    x$timestamp[1] <- start
+  }
+
+  end <- tail(x[[ts_col]], 1)
+  if (end_of_day) {
+    end <- end + days(1)
+    minute(end) <- hour(end) <- second(end) <- 0
+    end <- end - seconds(1)
+  } 
+    
+  interval_iter(x, ts_col, start, end, hours(1) - seconds(1))
 }
