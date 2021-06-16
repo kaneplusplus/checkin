7b23389c217d48057838310b8fb27119a4a09a06
kaneplusplus <kaneplusplus@gmail.com>
2021-06-16 19:37:48 -0400

    Fixing documentation.

--- R/checkins-data.r
+++ R/checkins-data.r
@@ -2,11 +2,13 @@
 #'
 #' @description The checkins data set is an example of a checkin data set.
 #' It consists of three columns.
-#' \begin{enumerate}
-#' \item{[id]}{The identifier of a device or person checking into a location.}
-#' \item{[timestamp]}{The time when the checkin occured.}
-#' \item{[location]}{The identifier of where a checkin occured.}
-#' \end{enumerate}
+#'
+#' \itemize{
+#' \item{[id:] }{The identifier of a device or person checking into a location.}
+#' \item{[timestamp:] }{The time when the checkin occured.}
+#' \item{[location:] }{The identifier of where a checkin occured.}
+#' }
+#'
 #' Please note that this data set was simulated illustrative purposes only.
 #'
 #' @docType data
